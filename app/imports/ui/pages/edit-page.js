06dd1a049f41c355594a86fdc3b2a640a61fae62
Philip Johnson <johnson@hawaii.edu>
2016-09-26 15:22:15 -1000

    Simplified the form, but upon repeated refresh, still find
    Exception in template helper: TypeError: Cannot read property 'profile'
    of undefined
    
    Solution: publish and subscribe

--- app/imports/ui/pages/edit-page.js
+++ app/imports/ui/pages/edit-page.js
@@ -8,23 +8,27 @@ import { _ } from 'meteor/underscore';
 Template.Edit_Page.helpers({
   nameValue() {
     const studentData = StudentData.findOne(FlowRouter.getParam('_id'));
-    return studentData && studentData.name;
+    return studentData.name;
   },
   bioValue() {
     const studentData = StudentData.findOne(FlowRouter.getParam('_id'));
-    return studentData && studentData.bio;
+    return studentData.bio;
   },
   hobbyChecked(hobby) {
     const studentData = StudentData.findOne(FlowRouter.getParam('_id'));
-    return studentData && _.contains(studentData.hobbies, hobby) && true;
+    return _.contains(studentData.hobbies, hobby) && true;
   },
   levelChecked(level) {
     const studentData = StudentData.findOne(FlowRouter.getParam('_id'));
-    return studentData && (studentData.level === level) && true;
+    return (studentData.level === level) && true;
+  },
+  gpaSelected(gpa) {
+    const studentData = StudentData.findOne(FlowRouter.getParam('_id'));
+    return (studentData.gpa === gpa) && true;
   },
   majorSelected(major) {
     const studentData = StudentData.findOne(FlowRouter.getParam('_id'));
-    return studentData && _.contains(studentData.majors, major) && true;
+    return _.contains(studentData.majors, major) && true;
   },
 
 });
@@ -38,10 +42,35 @@ Template.Edit_Page.onRendered(function enableSemantic() {
   this.$('.ui.checkbox').checkbox();
   // Enable radio buttons (single selection)  (Level)
   this.$('.ui.radio.checkbox').checkbox();
-
-  this.autorun(() => {
-    const studentData = StudentData.findOne(FlowRouter.getParam('_id'));
-    this.$('.ui.selection.dropdown').dropdown('set selected', studentData.gpa);
-  });
 });
+
+Template.Edit_Page.events({
+  'submit .student-data-form'(event) {
+    event.preventDefault();
+    // Get name (text field)
+    const name = event.target.name.value;
+    // Get bio (text area).
+    const bio = event.target.bio.value;
+    // Get list of checked hobbies (checkboxes)
+    const hobbies = [];
+    _.each(['surfing', 'running', 'biking', 'paddling'], function setHobby(hobby) {
+      if (event.target[hobby].checked) {
+        hobbies.push(event.target[hobby].value);
+      }
+    });
+    // Radio buttons (Level)
+    const level = event.target.level.value;
+    // Drop down list (GPA)
+    const gpa = event.target.gpa.value;
+    // Multiple select list  (Majors)
+    const selectedMajors = _.filter(event.target.majors.selectedOptions, (option) => option.selected);
+    const majors = _.map(selectedMajors, (option) => option.value);
+
+    console.log('update', name, bio, hobbies, level, gpa, majors);
+    const id = StudentData.update(FlowRouter.getParam('_id'), { name, bio, hobbies, level, gpa, majors });
+    console.log(id);
+  },
+});
+
+
 
