b3481c2bf62174289f3fd85bfe89a95002489e04
Philip Johnson <johnson@hawaii.edu>
2016-09-29 07:18:23 -1000

    figured out schema problem; needed cleaning.

--- app/imports/ui/pages/home-page.js
+++ app/imports/ui/pages/home-page.js
@@ -3,25 +3,54 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 import { _ } from 'meteor/underscore';
 import { ValidationError } from 'meteor/mdg:validation-error';
 import { StudentData, StudentDataSchema } from '../../api/studentdata/studentdata.js';
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+
 
 /* eslint-disable no-param-reassign */
 
 const displaySuccessMessage = 'displaySuccessMessage';
 
+const testSchema = new SimpleSchema({
+  name: {
+    label: 'Name',
+    type: String,
+    max: 5,
+  },
+});
+
+export const testCollection = new Mongo.Collection('TestCollection');
+
+testCollection.attachSchema(testSchema);
+
+console.log('starting up client');
+
+const schemaContext = testSchema.namedContext('testContext');
+
+const goodObject = { name: 'foo' };
+const badObject = { name: 'foobar' };
+
+console.log('validate good object4', schemaContext.validate(goodObject));
+console.log('validate bad object', schemaContext.validate(badObject));
+
+const testit = StudentDataSchema.namedContext('testit');
+
+console.log('validate bad object against studentdataschema', testit.validate(badObject));
+
 
 Template.Home_Page.onCreated(function onCreated() {
   this.messageFlags = new ReactiveDict();
+
 });
 
 Template.Home_Page.helpers({
   displaySuccessMessage() {
     const instance = Template.instance();
-    console.log('in helper displaySuccessMessage', displaySuccessMessage);
     return instance.messageFlags.get(displaySuccessMessage);
   },
   successClass() {
     const instance = Template.instance();
-    console.log('in helper successClass', displaySuccessMessage);
     return instance.messageFlags.get(displaySuccessMessage) ? 'success' : '';
   },
 });
@@ -37,7 +66,7 @@ Template.Home_Page.onRendered(function enableSemantic() {
 
 
 Template.Home_Page.events({
-  'submit .student-data-form'(event) {
+  'submit .student-data-form'(event, instance) {
     event.preventDefault();
     // Get name (text field)
     const name = event.target.name.value;
@@ -59,17 +88,32 @@ Template.Home_Page.events({
     const majors = _.map(selectedMajors, (option) => option.value);
 
     const newStudentData = { name, bio, hobbies, level, gpa, majors };
-    try {
-      console.log('about to validate', newStudentData);
-      // const result = StudentDataSchema.validate(newStudentData);
-      const result = StudentData.simpleSchema().namedContext().validate(newStudentData, { modifier: false });
-      console.log('after validation', result);
-      const id = StudentData.insert(newStudentData);
-      console.log('in event submit', displaySuccessMessage, id);
-      Template.instance().messageFlags.set(displaySuccessMessage, id);
-    } catch (err) {
-      console.log('validation not ok', err);
-    }
+
+    // const testit2 = StudentDataSchema.namedContext('testit2');
+    //
+    // console.log('validate bad object against studentdataschema2', testit2.validate(badObject));
+
+    console.log('reset validation');
+    const contextName = `context-${new Date()}`;
+    console.log(contextName);
+    const context = StudentDataSchema.namedContext(contextName);
+    context.resetValidation();
+    console.log('before cleaning', newStudentData);
+    StudentDataSchema.clean(newStudentData);
+    console.log('after cleaning', newStudentData);
+    const isValid = context.validate(newStudentData);
+    console.log('schema validity result', isValid);
+    // // try {
+    //   console.log('about to validate', newStudentData);
+    //   // const result = StudentDataSchema.validate(newStudentData);
+    //   const result = StudentData.simpleSchema().namedContext().validate(newStudentData, { modifier: false });
+    //   console.log('after validation', result);
+    //   const id = StudentData.insert(newStudentData);
+    //   console.log('in event submit', displaySuccessMessage, id);
+    //   Template.instance().messageFlags.set(displaySuccessMessage, id);
+    // } catch (err) {
+    //   console.log('validation not ok', err);
+    // }
   },
 });
 
